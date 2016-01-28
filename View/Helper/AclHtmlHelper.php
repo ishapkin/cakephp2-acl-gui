6067f557e799f36130a478b27c137519ec5c9c6e
Ilya Shapkin <info@shapkin.org>
2016-01-29 01:09:25 +0300

    Add "check" method for check url access

--- View/Helper/AclHtmlHelper.php
+++ View/Helper/AclHtmlHelper.php
@@ -3,19 +3,19 @@ App::uses('View/Helper', 'View/HtmlHelper');
 
 class AclHtmlHelper extends HtmlHelper
 {
-    var $helpers = array('Session');
-    
+    public $helpers = array('Session');
+
+    /**
+     * Генерация ссылки с проверкой доступа по ролевой модели
+     * @param string $title
+     * @param null $url
+     * @param array $options
+     * @param bool $confirmMessage
+     * @return null|string
+     */
     function link($title, $url = null, $options = array(), $confirmMessage = false)
     {
-        $permissions = $this->Session->read('Alaxos.Acl.permissions');
-        if(!isset($permissions))
-        {
-            $permissions = array();
-        }
-
-        $aco_path = AclRouter :: aco_path($url);
-        
-        if(isset($permissions[$aco_path]) && $permissions[$aco_path] == 1)
+        if($this->_aclCheck($url))
         {
             return parent::link($title, $url, $options, $confirmMessage);
         }
@@ -23,5 +23,42 @@ class AclHtmlHelper extends HtmlHelper
         {
             return null;
         }
+    }
+
+    /**
+     * Проверка доступа по ACL
+     * @param $url
+     * @return bool
+     */
+
+    function _aclCheck($url) {
+        $permissions = $this->Session->read('Alaxos.Acl.permissions');
+        if(!isset($permissions))
+        {
+            $permissions = array();
+        }
+
+        $aco_path = AclRouter :: aco_path($url);
+
+        return isset($permissions[$aco_path]) && $permissions[$aco_path] == 1;
+    }
+
+    /**
+     * Проверка группы экшенов для списков меню
+     * @param $url
+     * @return bool
+     */
+    function check($url)
+    {
+        $access = false;
+
+        if(is_array($url)) {
+            foreach ($url as $item) {
+                $access &= $this->_aclCheck($item);
+            }
+        } else {
+            $access = $this->_aclCheck($url);
+        }
+        return $access;
     }
 }
